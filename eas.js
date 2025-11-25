d4da62c902d906826375e668216d5f75e3a71969
foxxt1 <tajfox07@icloud.com>
2025-11-25 14:43:02 +1100

    Create working button for number of squares on each side with hovering effects working, and amount not being num on each side, but total instead

--- eas.js
+++ eas.js
@@ -1,19 +1,58 @@
+// container declaration before func so that the div elements can have somewere to be put
 let container = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++) {
-    let newDiv = document.createElement("div");
-    newDiv.classList = "newDiv";
-    newDiv.textContent = i + 1;
-    container.appendChild(newDiv);
+function generateGrid(amount) {
+    for (let i = 0; i < amount; i++) {
+        let newDiv = document.createElement("div");
+        newDiv.classList = "newDiv";
+        newDiv.textContent = i + 1;
+        // newDiv.addEventListener("mouseover", changeOnHover)
+        container.appendChild(newDiv);
+    }
 }
+generateGrid(16);
 
+// newDiv declaration so that the previously generated divs can be seen and counted
 let newDiv = document.querySelectorAll(".newDiv");
-
-
-
-for (let j = 0; j < newDiv.length; j++) {
-    console.log(j, newDiv[j]);
-    newDiv[j].addEventListener("mouseover", function () {
-    (newDiv[j].classList.contains("newDivHover")) ? newDiv[j].classList = newDiv[j].classList : newDiv[j].classList += " newDivHover";
-    });
+function divEventListeners() {
+    for (let j = 0; j < newDiv.length; j++) {
+        newDiv[j].addEventListener("mouseover", function () {
+        (newDiv[j].classList.contains("newDivHover")) ? newDiv[j].classList = newDiv[j].classList : newDiv[j].classList += " newDivHover";
+        });
+    }
 }
+divEventListeners();
+
+
+
+
+function changeOnHover() {
+    (newDiv.classList.contains("newDivHover")) ? newDiv.classList = newDiv.classList : newDiv.classList += " newDivHover";
+}
+
+
+let sideSquareCountButton = document.querySelector(".sideSquareCount")
+sideSquareCountButton.addEventListener("click", onClick)
+    
+    
+function onClick() {
+    let inputAmount = prompt("How many squares would you like per side? \n (Max 100)", 16)
+
+    console.log(newDiv.length);
+
+    for (let x = 0; x < newDiv.length; x++) {
+        console.log(newDiv[x], x)
+        // container.removeChild(newDiv[x]);
+        container.innerHTML = ""
+    }
+    generateGrid(inputAmount);
+    newDiv = document.querySelectorAll(".newDiv")
+    divEventListeners();
+}
+
+
+
+
+
+
+
