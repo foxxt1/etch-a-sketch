b67bb7d76569ed7f809305b614e153189dcf5dcf
foxxt1 <tajfox07@icloud.com>
2025-11-25 16:39:12 +1100

    Finish Etch-A-Sketch styling and functionality

--- eas.js
+++ eas.js
@@ -1,24 +1,47 @@
 // container declaration before func so that the div elements can have somewere to be put
 let container = document.querySelector(".container");
 
+const root = document.querySelector(":root");
+
 function generateGrid(amount) {
-    for (let i = 0; i < amount; i++) {
+
+    if (amount > 100) {
+        alert("That amount was greater than 100. Setting to 100.")
+        amount = 100
+    }
+    root.style.setProperty("--squareNumPerSide", amount);
+
+    const display = document.querySelector(".sideSquareDisplay")
+    display.textContent = `${amount} squares per side`
+
+    for (let i = 0; i < (amount * amount); i++) {
         let newDiv = document.createElement("div");
         newDiv.classList = "newDiv";
-        newDiv.textContent = i + 1;
+        // newDiv.textContent = i + 1;
         // newDiv.addEventListener("mouseover", changeOnHover)
         container.appendChild(newDiv);
     }
 }
-generateGrid(16);
+generateGrid(4);
 
 // newDiv declaration so that the previously generated divs can be seen and counted
 let newDiv = document.querySelectorAll(".newDiv");
 function divEventListeners() {
     for (let j = 0; j < newDiv.length; j++) {
         newDiv[j].addEventListener("mouseover", function () {
-        (newDiv[j].classList.contains("newDivHover")) ? newDiv[j].classList = newDiv[j].classList : newDiv[j].classList += " newDivHover";
+        (newDiv[j].classList.contains("newDivHover")) ?
+        (newDiv[j].classList = newDiv[j].classList) : (
+            newDiv[j].classList += " newDivHover"
+            // ,root.style.setProperty("--filledInNum", randomColor())
+        );
         });
+        // newDiv[j].addEventListener("mouseover", function ()  {
+        //     if (newDiv[j].classList.contains("newDivHover")) {
+        //         (newDiv[j].classList = newDiv[j].classList, root.style.setProperty("--filledInNum", randomColor()))
+        //     } else if {
+
+        //     }
+        // }
     }
 }
 divEventListeners();
@@ -28,6 +51,7 @@ divEventListeners();
 
 function changeOnHover() {
     (newDiv.classList.contains("newDivHover")) ? newDiv.classList = newDiv.classList : newDiv.classList += " newDivHover";
+    // root.style.setProperty("--filledInNum", ++)
 }
 
 
@@ -36,13 +60,9 @@ sideSquareCountButton.addEventListener("click", onClick)
     
     
 function onClick() {
-    let inputAmount = prompt("How many squares would you like per side? \n (Max 100)", 16)
-
-    console.log(newDiv.length);
+    let inputAmount = prompt("How many squares would you like per side? \n (Max 100)", 4)
 
     for (let x = 0; x < newDiv.length; x++) {
-        console.log(newDiv[x], x)
-        // container.removeChild(newDiv[x]);
         container.innerHTML = ""
     }
     generateGrid(inputAmount);
@@ -50,7 +70,28 @@ function onClick() {
     divEventListeners();
 }
 
+let resetButton = document.querySelector(".reset");
+resetButton.addEventListener("click", onResetClick);
 
+function onResetClick() {
+    // location.reload()
+    for (let x = 0; x < newDiv.length; x++) {
+        container.innerHTML = ""
+    }
+    generateGrid(4);
+    newDiv = document.querySelectorAll(".newDiv")
+    divEventListeners();
+}
+
+function randomColor() {
+    let color = []
+    let array = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
+    for (let i = 0; i < 6; i++) {
+        let num = Math.floor(Math.random()*16);
+        color.push(array[num]);
+    }
+    return `#${color.join("")}`;
+}
 
 
 
